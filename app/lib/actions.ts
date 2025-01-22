630922a1b3570bc1f7cba70f4d19c5a8adb0fefb
Jesus Contreras <jgcc26@gmail.com>
2025-01-22 13:29:03 -0400

    fixing eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { custom, z } from 'zod';
+import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
